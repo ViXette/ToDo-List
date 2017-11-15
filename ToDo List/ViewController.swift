481ae280cbc12c52d24bd436735e9efa5eb53e46
ViXette <v.xette@gmail.com>
2017-11-15 21:47:14 +1000

    Initial commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Created by ViXette on 15/11/2017.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+}
+
